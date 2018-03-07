e46d9f535da9bf2dfbbbddd0b18dd5dd1c977e7e
Gary Wu <garywu@users.noreply.github.com>
2018-03-07 13:25:56 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -79,16 +79,16 @@ sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
 #============
 # GeckoDriver
 #============
-ARG GECKODRIVER_VERSION=latest
-RUN GK_VERSION=$(if [ ${GECKODRIVER_VERSION:-latest} = "latest" ]; then echo $(wget -qO- "https://api.github.com/repos/mozilla/geckodriver/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([0-9.]+)".*/\1/'); else echo $GECKODRIVER_VERSION; fi) \
-  && echo "Using GeckoDriver version: "$GK_VERSION \
-  && wget --no-verbose -O /tmp/geckodriver.tar.gz https://github.com/mozilla/geckodriver/releases/download/v$GK_VERSION/geckodriver-v$GK_VERSION-linux64.tar.gz \
-  && rm -rf /opt/geckodriver \
-  && tar -C /opt -zxf /tmp/geckodriver.tar.gz \
-  && rm /tmp/geckodriver.tar.gz \
-  && mv /opt/geckodriver /opt/geckodriver-$GK_VERSION \
-  && chmod 755 /opt/geckodriver-$GK_VERSION \
-  && ln -fs /opt/geckodriver-$GK_VERSION /usr/bin/geckodriver
+GECKODRIVER_VERSION=latest
+GK_VERSION=$(if [ ${GECKODRIVER_VERSION:-latest} = "latest" ]; then echo $(wget -qO- "https://api.github.com/repos/mozilla/geckodriver/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([0-9.]+)".*/\1/'); else echo $GECKODRIVER_VERSION; fi)
+echo "Using GeckoDriver version: "$GK_VERSION
+wget --no-verbose -O /tmp/geckodriver.tar.gz https://github.com/mozilla/geckodriver/releases/download/v$GK_VERSION/geckodriver-v$GK_VERSION-linux64.tar.gz
+sudo rm -rf /opt/geckodriver
+sudo tar -C /opt -zxf /tmp/geckodriver.tar.gz
+rm /tmp/geckodriver.tar.gz
+sudo mv /opt/geckodriver /opt/geckodriver-$GK_VERSION
+sudo chmod 755 /opt/geckodriver-$GK_VERSION
+sudo ln -fs /opt/geckodriver-$GK_VERSION /usr/bin/geckodriver
 
 # Finalize apt-get dependancies
 sudo apt-get -f install -y --force-yes
